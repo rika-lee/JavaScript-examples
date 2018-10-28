ca0bc6bdd250fbdec277f9d33d5407f64fb58f9b
rika-lee <mktr.rika@gmail.com>
2018-10-28 19:20:24 +0900

    가위 바위 보 게임 수정
    사용자가 잘못입력했다가 '확인' 누르면 다시 가위바위보 입력하는 곳으로 돌아감

--- my-project/rock-paper-scissors/js/scripts.js
+++ my-project/rock-paper-scissors/js/scripts.js
@@ -1,40 +1,42 @@
 // 유저의 패
 var userInput = prompt('가위, 바위, 보!');
 
-if (userInput !== '가위' && userInput !== '바위' && userInput !== '보') {
+
+while (userInput !== '가위' && userInput !== '바위' && userInput !== '보') {
     alert('가위, 바위, 보 중 하나를 입력해야 합니다.');
-} else {
+    var userInput = prompt('가위, 바위, 보!');
+}
 
 // 컴퓨터의 패
-    var comInput;
+var comInput;
 
-    var rnd = Math.random();
+var rnd = Math.random();
 
-    if (rnd < 0.33) {
-        comInput = '가위';
-    } else if (rnd < 0.66) {
-        comInput = '바위';
+if (rnd < 0.33) {
+    comInput = '가위';
+} else if (rnd < 0.66) {
+    comInput = '바위';
+} else {
+    comInput = '보';
+}
+
+if (userInput === '가위') {
+    if (comInput === '가위') {
+        alert('컴퓨터의 패 : ' + comInput + ' "무승부"');
+    } else if (comInput === '바위') {
+        alert('컴퓨터의 패 : ' + comInput + ' "컴퓨터 승"');
     } else {
-        comInput = '보';
+        alert('컴퓨터의 패 : ' + comInput + ' "당신 승"');
     }
-
-    if (userInput === '가위') {
-      if (comInput === '가위') {
-        alert('컴퓨터의 패 : ' + comInput + ' "무승부"');
-      } else if (comInput === '바위') {
-        alert('컴퓨터의 패 : ' + comInput + ' "컴퓨터 승"');
-      } else {
+} else if (userInput === '바위') {
+    if (comInput === '가위') {
         alert('컴퓨터의 패 : ' + comInput + ' "당신 승"');
-      }
-    } else if (userInput === '바위') {
-      if (comInput === '가위') {
-        alert('컴퓨터의 패 : ' + comInput + ' "당신 승"');
-      } else if (comInput === '바위') {
+    } else if (comInput === '바위') {
         alert('컴퓨터의 패 : ' + comInput + ' "무승부"');
-      } else {
-        alert('컴퓨터의 패 : ' + comInput + ' "컴퓨터 승"');
-      }
     } else {
+        alert('컴퓨터의 패 : ' + comInput + ' "컴퓨터 승"');
+    }
+} else {
     if (comInput === '가위') {
       alert('컴퓨터의 패 : ' + comInput + ' "컴퓨터 승"');
     } else if (comInput === '바위') {
@@ -42,8 +44,8 @@ if (userInput !== '가위' && userInput !== '바위' && userInput !== '보') {
     } else {
       alert('컴퓨터의 패 : ' + comInput + ' "무승부"');
     }
-    }
-
-
 }
+
+
+//}
 // console.log('컴퓨터의 패:', comInput);
