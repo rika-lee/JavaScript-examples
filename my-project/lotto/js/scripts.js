35771851fe08122e20186f384a5e99bcba441a92
rika-lee <mktr.rika@gmail.com>
2018-11-13 21:04:40 +0900

    내맘대로 로또 번호 생성기

--- my-project/lotto/js/scripts.js
+++ my-project/lotto/js/scripts.js
@@ -13,10 +13,13 @@ for (var i = 0; i < 6; i++) {
     result.push(num);
 }
 
-result.sort(function(a, b) {
-    return a-b;
-});
+document.write('<div><h1>이번주 당첨 번호는...</h1></div>');
+
+var order = ['첫', '두', '세', '네', '다섯', '여섯'];
 
 for (var i = 0; i < 6; i++) {
-    document.write('<span class="ball">' + result[i] + '</span>');
+    document.write(
+        '<div><h2>' + order[i] + '번째 번호</h2>'
+        + '<span class="ball">' + result[i] + '</span></div>'
+    );
 }
