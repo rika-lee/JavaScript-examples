682e581d58e0de084af5207f68dbdc0da6aa4034
rika-lee <mktr.rika@gmail.com>
2018-11-25 18:54:38 +0900

    농구 게임 리팩토링 및 재도전 추가

--- my-project/basketball/js/scripts.js
+++ my-project/basketball/js/scripts.js
@@ -3,44 +3,64 @@ var userScore = 0;
 var isComputerTurn = true;
 var shotsLeft = 15;
 
+function showText(s) {
+    var textElem = document.getElementById('text');
+    textElem.innerHTML = s;
+}
+
+function updateComputerScore(score) {
+    comScore += score;
+    var comScoreElem = document.getElementById('computer-score');
+    comScoreElem.innerHTML = comScore;
+}
+
+function updateUserScore(score) {
+    userScore += score;
+    var userScoreElem = document.getElementById('user-score');
+    userScoreElem.innerHTML = userScore;
+}
+
+function disableComputerButtons(flag) {
+    var computerButtons = document.getElementsByClassName('btn-computer');
+
+    for (var i = 0; i < computerButtons.length; i++) {
+        computerButtons[i].disabled = flag;
+    }
+}
+
+function disableUSerButtons(flag) {
+    var userButtons = document.getElementsByClassName('btn-user');
+
+    for (var i=0; i < userButtons.length; i++) {
+        userButtons[i].disabled = flag;
+    }
+}
+
 function onComputerShoot() {
     if (!isComputerTurn)
         return;
 
-    var textElem = document.getElementById('text');
-    var comScoreElem = document.getElementById('computer-score');
     var shootType = Math.random() < 0.5 ? 2 : 3;
 
     if (shootType === 2) {
         if (Math.random() < 0.5) {
-            textElem.innerHTML = '컴퓨터 2점 슛 성공!';
-            comScore += 2;
-            comScoreElem.innerHTML = comScore;
+            showText('컴퓨터 2점 슛 성공!');
+            updateComputerScore(2);
         } else {
-            textElem.innerHTML = '컴퓨터 2점 슛 실패';
+            showText('컴퓨터 2점 슛 실패');
         }
     } else {
         if (Math.random() < 0.33) {
-            textElem.innerHTML = '컴퓨터 3점 슛 성공!';
-            comScore += 3;
-            comScoreElem.innerHTML = comScore;
+            showText('컴퓨터 3점 슛 성공!');
+            updateComputerScore(3);
         } else {
-            textElem.innerHTML = '컴퓨터 3점 슛 실패';
+            showText('컴퓨터 3점 슛 실패');
         }
     }
     isComputerTurn = false;
 
-    var computerButtons = document.getElementsByClassName('btn-computer');
-
-    for (var i = 0; i < computerButtons.length; i++) {
-        computerButtons[i].disabled = true;
-    }
-
-    var userButtons = document.getElementsByClassName('btn-user');
-
-    for (var i = 0; i < userButtons.length; i++){
-        userButtons[i].disabled = false;
-    }
+    disableComputerButtons(true);
+    disableUSerButtons(false);
 }
 
 function onUserShoot(shootType) {
@@ -48,39 +68,26 @@ function onUserShoot(shootType) {
     if (isComputerTurn)
         return;
 
-    var textElem = document.getElementById('text');
-    var userScoreElem = document.getElementById('user-score');
-
     if (shootType === 2) {
         if (Math.random() < 0.5) {
-            textElem.innerHTML = '2점 슛 성공!';
-            userScore += 2;
-            userScoreElem.innerHTML = userScore;
+            showText('2점 슛 성공!');
+            updateUserScore(2);
         } else {
-            textElem.innerHTML = '2점 슛 실패';
+            showText('2점 슛 실패');
         }
     } else {
         if (Math.random() < 0.33) {
-            textElem.innerHTML = '3점 슛 성공!';
-            userScore += 3;
-            userScoreElem.innerHTML = userScore;
+            showText('3점 슛 성공!');
+            updateUserScore(3);
         } else {
-            textElem.innerHTML = '3점 슛 실패';
+            showText('3점 슛 실패');
         }
     }
     isComputerTurn = true;
 
-    var computerButtons = document.getElementsByClassName('btn-computer');
+    disableComputerButtons(false);
+    disableUSerButtons(true);
 
-    for (var i = 0; i < computerButtons.length; i++) {
-        computerButtons[i].disabled = false;
-    }
-
-    var userButtons = document.getElementsByClassName('btn-user');
-
-    for (var i = 0; i < userButtons.length; i++) {
-        userButtons[i].disabled = true;
-    }
     shotsLeft--;
 
     var shotsLeftElem = document.getElementById('shots-left');
@@ -88,18 +95,22 @@ function onUserShoot(shootType) {
 
     if (shotsLeft === 0) {
         if  (userScore > comScore)
-            textElem.innerHTML = '승리했습니다!';
+            showText('승리했습니다!');
         else if (userScore < comScore)
-            textElem.innerHTML = '아쉽게도 졌습니다...';
+            showText('아쉽게도 졌습니다...');
         else
-            textElem.innerHTML = '비겼습니다.';
+            showText('비겼습니다.');
 
-        for (var i = 0; i < computerButtons.length; i++) {
-            computerButtons[i].disabled = true;
-        }
-
-        for (var i =0; i < userButtons.length; i++) {
-            userButtons[i].disabled = true;
-        }
+        disableComputerButtons(true);
+        disableUSerButtons(true);
+        
+        var refreshButtons = document.getElementsByClassName('btn-refresh');
+        for (i = 0; i < refreshButtons.length; i++) {
+        refreshButtons[i].disabled = false;
+        }   
     }
+}
+
+function reloadPage() {
+    location.reload();
 }
