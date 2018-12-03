30c9b1d3138d9b5087da58e233e10ead82020679
rika-lee <mktr.rika@gmail.com>
2018-12-04 05:25:15 +0900

    별찍기 업데이트2

--- my-project/star/js/1.js
+++ my-project/star/js/1.js
@@ -123,3 +123,31 @@ for (i = 0; i < 9; i++) {
     }
 } 
 console.log(stars);
+
+var stars = '';
+for (i = 0; i < 9; i++) {
+    if (i < 5) {
+        for (j = 3; j >= i; j--) {
+            stars += '*';
+        }
+        for (k = 0; k <= 2*i; k++) {
+            stars += ' ';
+        }
+        for (l = 3; l >= i; l--) {
+            stars += '*';
+        }
+        stars += '\n';
+    } else {
+        for (j = 5; j <= i; j++) {
+            stars += '*';
+        }
+        for (k = 16; k >= 2*i; k--) {
+            stars += ' ';
+        }
+        for (l = 5; l <= i; l++) {
+            stars += '*';
+        }
+        stars += '\n';
+    }
+}
+console.log(stars);
