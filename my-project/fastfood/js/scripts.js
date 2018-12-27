3f831124e11676b732d9a6c91f79423b0dd7cc08
rika-lee <mktr.rika@gmail.com>
2018-12-27 21:48:44 +0900

    패스트푸드점 찾기

---
+++ my-project/fastfood/js/scripts.js
@@ -0,0 +1,38 @@
+var API_URL = 'https://floating-harbor-78336.herokuapp.com/fastfood';
+
+$(function() {
+    $('.btn-search').click(function() {
+        var searchKeyword = $('#txt-search').val();
+        search(searchKeyword);
+    });
+
+    $('#txt-search').on('keypress', function(e) {
+        if (e.keyCode === 13) {
+            $('.btn-search').trigger('click');
+        }
+    });
+});
+
+function search(searchKeyword) {
+    $.get(API_URL, {
+        searchKeyword: searchKeyword
+    }, function(data) {
+        var list = data.list;
+        var total = data.total;
+        $('.total').html('총 ' + total + '개의 패스트푸드점을 찾았습니다.');
+
+        var $list = $('.list');
+        for (var i = 0; i < list.length; i++) {
+            var item = list[i];
+
+            var $elem = $('#item-template')
+                .clone()
+                .removeAttr('id');
+            $elem.find('.item-no').html(i + 1);
+            $elem.find('.item-name').html(item.name);
+            $elem.find('.item-addr').html(item.addr);
+            
+            $list.append($elem);
+        }
+    });
+}
