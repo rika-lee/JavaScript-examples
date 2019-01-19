7542d2dd1018db994954d2f8517f139ab69a4d6d
rika-lee <mktr.rika@gmail.com>
2019-01-19 14:32:37 +0900

    마이트립 이미지 갤러리 추가하기

--- my-project/my-trip-advisor/scripts/detail.js
+++ my-project/my-trip-advisor/scripts/detail.js
@@ -13,6 +13,17 @@ function getDetail(id) {
         $('.detail-header-name').html(r.name);
         $('.detail-header-city-name').html(r.cityName);
         $('.detail-desc-text').html(r.desc);
+
+        var $galleria = $('#detail-images');
+        var images = r.subImageList;
+
+        for ( var i = 0; i < images.length; i++) {
+            var $image = $('<img src="' + images[i] + '" />');
+            $galleria.append($image);
+        }
+
+        Galleria.loadTheme('libs/galleria/themes/classic/galleria.classic.min.js');
+        Galleria.run('#detail-images');
     });
 }
 
