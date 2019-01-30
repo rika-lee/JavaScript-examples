0d99dbde6dae18fb8948f6267a8083d065949d98
rika-lee <mktr.rika@gmail.com>
2019-01-30 09:50:21 +0900

    마이트립 지도 추가하기

--- my-project/my-trip-advisor/scripts/detail.js
+++ my-project/my-trip-advisor/scripts/detail.js
@@ -1,8 +1,35 @@
+var map;
+
 $(function() {
     var id = parseId(window.location.search);
 
     getDetail(id);
+
+    showMap();
 });
+
+function showMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: {
+            lat: 33.3627,
+            lng: 126.5292
+        }
+    });
+}
+
+function showMarker(lat, lng) {
+var pos = {
+    lat: lat,
+    lng: lng
+};
+
+    new google.maps.Marker({
+        position: pos,
+        map: map
+    });
+    map.panTo(pos);
+}
 
 function getDetail(id) {
     var url = 'https://javascript-basic.appspot.com/locationDetail';
@@ -24,6 +51,8 @@ function getDetail(id) {
 
         Galleria.loadTheme('libs/galleria/themes/classic/galleria.classic.min.js');
         Galleria.run('#detail-images');
+
+        showMarker(r.position.x, r.position.y);
     });
 }
 
