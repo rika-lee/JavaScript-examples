936698001770163f320318d3ebc7308a0aafe975
rika-lee <mktr.rika@gmail.com>
2019-02-05 18:01:25 +0900

    마이트립 마이페이지 목록에서 여행지 삭제하기

--- my-project/my-trip-advisor/scripts/mytrips.js
+++ my-project/my-trip-advisor/scripts/mytrips.js
@@ -1,4 +1,6 @@
+var MARKER_LABEL = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var map;
+var markers = {};
 
 $(function() {
     var myTrips = Cookies.getJSON('MYTRIPS');
@@ -11,17 +13,45 @@ $(function() {
 });
 
 function generateMyTripList(list) {
+    var bounds = new google.maps.LatLngBounds();
     var $list = $('#mytrip-list');
 
     for (var i = 0; i < list.length; i++) {
         var myTrip = list[i];
 
+        var pos = {
+            lat: myTrip.x,
+            lng: myTrip.y
+        };
+
+        var markerLabel = MARKER_LABEL[i];
         var $item = $('#mytrip-item-template').clone().removeAttr('id');
         
-        $item.find('.item-name').html(myTrip.name);
+        $item.data('id', myTrip.id);
+        $item.find('.item-name').html(markerLabel + '. ' + myTrip.name);
         $item.find('.item-city-name').html(myTrip.cityName);
+        $item.find('.item-remove').click(function() {
+            var $elem = $(this).closest('.mytrip-item');
+            var id = $elem.data('id');
+
+            $elem.remove();
+
+            markers[id].setMap(null);
+            markers[id] = null;
+        });
         $list.append($item);
+
+        var marker = new google.maps.Marker({
+            position: pos,
+            label: markerLabel,
+            map: map
+        });
+
+        markers[myTrip.id] = marker;
+
+        bounds.extend(pos);
     }
+    map.fitBounds(bounds);
 }
 
 function showMap() {
