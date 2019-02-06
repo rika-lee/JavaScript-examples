e313b1f22b15493efa0f9e1e82fa6a8da9f37a93
rika-lee <mktr.rika@gmail.com>
2019-02-06 17:02:15 +0900

    마이트립 회원가입 페이지 검사 로직 추가

--- my-project/my-trip-advisor/scripts/register.js
+++ my-project/my-trip-advisor/scripts/register.js
@@ -15,6 +15,41 @@ $(function() {
             $('#inp-email').next().html('잘못된 형식입니다').show();
             return;
         }
+
+        var password = $('#inp-password').val();
+
+        if (!validatePassword(password)) {
+            $('#inp-password').next().html('대문자와 숫자가 포함된 최소 8자의 문자열이어야 합니다').show();
+            return;
+        }
+
+        var confirm = $('#inp-confirm').val();
+
+        if (password !== confirm) {
+            $('#inp-confirm').next().html('비밀번호가 일치하지 않습니다').show();
+            return;
+        }
+
+        var gender = $('input[name="gender"]:checked').val();
+
+        if (!gender) {
+            $('#inp-female').siblings('.txt-warning').html('필수 항목입니다').show();
+            return;
+        }
+
+        var birth = $('#sel-birth').val();
+
+        if (!birth) {
+            $('#sel-birth').siblings('.txt-warning').html('필수 항목입니다').show();
+            return;
+        }
+
+        var accept = $('#inp-accept:checked').val();
+
+        if (!accept) {
+            $('#inp-accept').next().next().html('필수 항목입니다').show();
+            return;
+        }
     });
 });
 
@@ -27,4 +62,9 @@ function generateYears($select) {
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+
+function validatePassword(password) {
+    var re = /^(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
+    return re.test(password);
 }
